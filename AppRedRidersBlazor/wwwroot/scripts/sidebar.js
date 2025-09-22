e5c19911ab98604169296631a2617c60a42fd2de
KeynnerDFS <147161794+KeynnerDFS@users.noreply.github.com>
2025-09-22 11:56:19 -0400

    Progresso do Lucas

---
+++ AppRedRidersBlazor/wwwroot/scripts/sidebar.js
@@ -0,0 +1,11 @@
+﻿const btnActiveLists = document.querySelector("#btn-active-lists")
+const ulList = document.querySelector("#menu-lists")
+
+ulList.style.display = 'none'
+
+btnActiveLists.addEventListener("click", () => {
+    if (ulList.style.display === 'flex') {
+        return ulList.style.display = 'none'
+    }
+    ulList.style.display = 'flex'
+})
