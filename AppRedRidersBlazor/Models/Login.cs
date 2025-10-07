8e45c0f039999e69609a302636253b5287969028
KeynnerDFS <savikeynnerbr@gmail.com>
2025-10-07 16:28:12 -0400

    model login corrigido

--- AppRedRidersBlazor/Models/Login.cs
+++ AppRedRidersBlazor/Models/Login.cs
@@ -1,9 +1,9 @@
-﻿namespace AppRedRidersBlazor.Models;
+﻿namespace AppRedRidersBlazor.Models
 {
     public class Login
     {
-     public int Id { get; set; }
-     public string? email { get; set; }
-     public string? senha { get; set; }
+        public int Id { get; set; }
+        public string? email { get; set; }
+        public string? senha { get; set; }
     }
 }
